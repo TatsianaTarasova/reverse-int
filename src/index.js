a58e14b8371ae55352183b3e0a4cf7abab079811
TatsianaTarasova <70853063+TatsianaTarasova@users.noreply.github.com>
2020-10-20 18:10:24 +0200

    init : result task reverse

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function reverse (n) {
-  
+     if (n > 0) {
+       return +String(n).split('').reverse().join('');
+    }
+   else {
+       n = n * (-1);
+      return +String(n).split('').reverse().join('');
+    } 
 }
